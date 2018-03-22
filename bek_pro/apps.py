6d96f90771e9b41614aa6c389b897533ee7349b6
SinghSatbir <satbirform@gmail.com>
2018-03-22 09:33:55 +0530

    templates views and models

---
+++ bek_pro/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BekProConfig(AppConfig):
+    name = 'bek_pro'
---
+++ bek_pro/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django.template import loader
+
+
+def index(request):
+        template=loader.get_template('index.html')
+        context={
+            'message':'hello world'
+        }
+        return HttpResponse(template.render(context,request))
