4f1bff731d42a02e60bd0466a940297681f78f2d
SinghSatbir <satbirform@gmail.com>
2018-03-22 11:46:16 +0530

    admin site

--- bek_pro/admin.py
+++ bek_pro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from bek_pro.models import Item
+
+admin.site.register(Item)
--- bek_pro/views.py
+++ bek_pro/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.template import loader
 
+from bek_pro.models import Item
+
 
 def index(request):
         template=loader.get_template('index.html')
         context={
-            'message':'hello world'
+            'variable1':Item.objects.get(id=1),
+            #variable to store items
         }
         return HttpResponse(template.render(context,request))
